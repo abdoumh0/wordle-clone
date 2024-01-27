3de31aa3733dfe77d7074fed48a3fda33b398a07
Abdou Mehellel <abdoumeh421@gmail.com>
2024-01-27 13:57:13 +0000

    fixed backspace / added compare function to lib

---
+++ lib/compare.tsx
@@ -0,0 +1,42 @@
+export function Compare(word: string, input: string): number[] {
+  const LENGTH = word.length;
+  let pattern: number[] = new Array(LENGTH);
+  if (LENGTH != input.length) {
+    console.log("word length mismatch");
+    return pattern;
+  }
+  let flags: boolean[] = new Array(LENGTH);
+
+  for (let index = 0; index < LENGTH; index++) {
+    if (word[index] == input[index]) {
+      pattern[index] = 1;
+      flags[index] = true;
+    } else {
+      pattern[index] = 0;
+      flags[index] = false;
+    }
+  }
+
+  for (let index = 0; index < LENGTH; index++) {
+    if (pattern[index] != 0) {
+      continue;
+    } else {
+      for (let index_ = 0; index_ < LENGTH; index_++) {
+        if (flags[index_]) {
+          continue;
+        } else if (input[index] == word[index_]) {
+          flags[index_] = true;
+          pattern[index] = 2;
+        }
+      }
+    }
+  }
+
+  console.log(`comparing \"${word}\" & \"${input}\": [${pattern}]`);
+  console.log(
+    "0: doesnt exist; \t 1: right charecter; \t 2: right misplaced charecter"
+  );
+  return pattern;
+}
+
+Compare("hello", "ehlll");
